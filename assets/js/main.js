0a5a465a277f37c33bc2a0c4fd5f0503bd851ce8
Johnnatan Villaneda <joh.villaneda@outlook.com>
2021-11-03 01:58:51 -0500

    Working on qualification section

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,3 +28,38 @@ function linkAction() {
 }
 
 navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+/*==================== ACCORDION SKILLS ====================*/
+
+const skillsContent = document.getElementsByClassName(".skills__content"),
+  skillsHeader = document.querySelectorAll(".skills__header");
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+
+  if (itemClass === "skills__content skills__close") {
+    this.parentNode.className = "skills__content skills__open";
+  } else if (itemClass === "skills__content skills__open") {
+    this.parentNode.className = "skills__content skills__close";
+  }
+}
+
+skillsHeader.forEach((e) => {
+  e.addEventListener("click", toggleSkills);
+});
+
+/*==================== QUALIFICATION TABS ====================*/
+
+/*==================== SERVICES MODAL ====================*/
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+/*==================== TESTIMONIAL ====================*/
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/
+
+/*==================== SHOW SCROLL UP ====================*/
+
+/*==================== DARK LIGHT THEME ====================*/
